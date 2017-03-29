26c997117d38ad40ba6e029a6e5a2b01b7985dc1
Miladin Avric <avricmiladin995@gmail.com>
2017-03-29 15:00:45 +0200

    Moj drugi komit

---
+++ proba1/app/controllers/PoslovniPartneri.java
@@ -0,0 +1,66 @@
+package controllers;
+
+import java.util.List;
+
+import models.Drzava;
+import models.PoslovniPartner;
+import net.sf.oval.constraint.MaxSize;
+import play.data.validation.Required;
+import play.mvc.*;
+
+@With(Secure.class)
+public class PoslovniPartneri extends Controller {
+
+	public static void show(String mode){
+    	List<PoslovniPartner> poslovniPartneri = PoslovniPartner.findAll();
+    	if (mode == null || mode.equals(""))
+    		 mode = "edit";
+    	
+    	render(poslovniPartneri, mode);
+    }
+	
+	public static void add(@Required String tipPartnera,@Required String nazivPartnera,  @Required String pib, @Required String adresa) {
+		
+/*		if(validation.hasErrors()) {
+			for(play.data.validation.Error error : validation.errors()) {
+			System.out.println(error.message());
+			}
+			validation.keep();
+			show("add");
+		}else {*/
+			PoslovniPartner poslovniPartner = new PoslovniPartner();
+			poslovniPartner.tipPartnera = tipPartnera;
+			poslovniPartner.nazivPartnera = nazivPartnera;
+			poslovniPartner.pib = pib;
+			poslovniPartner.adresa = adresa;
+			poslovniPartner.save();
+			validation.keep();
+			show("add");
+	}
+    public static void edit(@Required String tipPartnera,@Required String nazivPartnera,  @Required String pib, @Required String adresa, long id) {
+    	PoslovniPartner pP = PoslovniPartner.findById(id);
+    	if(pP!=null){
+    	pP.tipPartnera = tipPartnera;
+    	pP.nazivPartnera = nazivPartnera;
+    	pP.pib = pib;
+    	pP.adresa = adresa;
+    	
+    	pP.save();
+    	}
+    	show("edit");
+    	}
+
+    public static void filter() {
+    		/*List<Drzava> drzave = Drzava.find("byNazivLikeAndOznakaLike", "%"+naziv+"%", "%"+oznaka+"%").fetch();
+    		String mode = "edit";
+    		renderTemplate("Drzave/show.html", drzave, mode);*/
+    	}
+        
+    	public static void remove(Long id){
+    		PoslovniPartner poslovniPartner = PoslovniPartner.findById(id);
+    		poslovniPartner.delete();
+    		show("");
+    	}
+    
+
+}
